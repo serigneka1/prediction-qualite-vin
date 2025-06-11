a1fa4106aab55c237cdbb8d124ceacd8899bd131
Serigne <kaserigne69@gmail.com>
2025-06-11 14:01:06 +0100

    feat: mise en place du projet

---
+++ README.md
@@ -0,0 +1,12 @@
+
+
+
+
+# Tracking
+```bash
+git init
+dvc init
+dvc add data
+dvc remote add myremote gdrive://1wP5nodWsBxjfi5G6dT3FuZxPGMLf-gng
+git add
+```
---
+++ steps/get_data.py
@@ -0,0 +1,17 @@
+import kagglehub
+import os
+import pandas as pd
+
+# Download latest version
+path = kagglehub.dataset_download("uciml/red-wine-quality-cortez-et-al-2009")
+
+print("Path to dataset files:", path)
+
+for file in os.listdir(path):
+    full_path = os.path.join(path, file)
+
+    df = pd.read_csv(full_path)
+    df.to_csv("../data/win_quality_red.csv")
+
+
+    
